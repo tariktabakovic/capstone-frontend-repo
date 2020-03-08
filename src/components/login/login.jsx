cb3111b613d9962ab138092e2a72422e6657151e
“tariktabakovic” <tariktab97@gmail.com>
2020-03-08 11:25:50 -0400

    implements reference for container

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -7,7 +7,7 @@ export class Login extends React.Component {
 
     render(){
         return (
-            <div className = "base-container">
+            <div className = "base-container" ref={this.props.containerRef}>
                 <div className = "header"> Login </div>
                 <div className = "content">
                     {/* <div className = "image"> 
