a416b55e903cf30cdc6777c2af62051fbc493663
“tariktabakovic” <tariktab97@gmail.com>
2020-03-08 10:36:02 -0400

    creates class for login component

---
+++ src/components/login/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class login extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            
+        )
+    }
+}
