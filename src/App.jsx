3ebb915ac1198dd453319c4c93c621470cd3ec3d
“tariktabakovic” <tariktab97@gmail.com>
2020-03-08 11:21:16 -0400

    changes App from function to classg

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.scss';
 import {Login} from './components/login/index';
 
-
-function App() {
-  return (
-    <div className= "App">
-      <Login/>
-    </div>    
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state= {
+    }
+  }
+  render(){
+    return()
+  }
 }
 
 export default App;
