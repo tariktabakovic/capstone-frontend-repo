9191a4204e6b9c54a4b819ad9ef5130b558ec3dc
“tariktabakovic” <tariktab97@gmail.com>
2020-03-08 11:33:28 -0400

    creates container for rightside animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import {Login} from './components/login/index';
+import {Login, Register} from './components/login/index';
 
 class App extends React.Component {
   constructor(props){
@@ -10,9 +10,34 @@ class App extends React.Component {
       isLoginActive: true
     }
   }
+
+  changeState(){
+    const {isLoginActive} = this.state;
+    // if(isLoginActive)
+  }
   render(){
-    return()
+    const {isLoginActive} = this.state;
+    const current = isLoginActive ? "Register" : "Login";
+    const currentActive = isLoginActive? "Login" : "Register";
+    return(
+      <div className = "App">
+        <div className = "login">
+          <div className = "container">
+            {isLoginActive && <Login containerRef={(ref) => this.current = ref}/> }
+            {!isLoginActive && <Register containerRef={(ref) => this.current = ref}/>}
+          </div>
+          <RightSide current= {current} containerRef={ref => this.RightSide = ref} onClick= {this.changeState.bind(this)}/>
+        </div>
+      </div>
+    )
   }
 }
 
+const RightSide = props =>{
+  return <div className = "right-side" ref= {props.containerRef} onClick= {props.onClick}>
+    <div className="inner-container">
+      <div className="text">{props.current}</div>
+    </div>
+  </div>
+}
 export default App;
