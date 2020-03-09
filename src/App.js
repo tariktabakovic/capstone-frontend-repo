e05523527d66446ec74d62e2c0816cf0d4f89edc
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 19:25:39 -0400

    imports Provider from react-redux & store

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import AppNavBar from './components/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ThoughtPatterns from './components/ThoughtPatterns';
+
+import {Provider} from 'react-redux';
+import store from './store';
+
 class App extends React.Component {
   
   render(){
