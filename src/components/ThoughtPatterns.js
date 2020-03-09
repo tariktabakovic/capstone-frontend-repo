480d89a933b8473bd7446abfbfed75b4d9fc5ddb
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 04:29:50 -0400

    maps static thoughts to ListGroup

--- src/components/ThoughtPatterns.js
+++ src/components/ThoughtPatterns.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Container, ListGroup, ListGroupitem, Button} from 'reactstrap';
+import {Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import uuid from 'uuid';
 
@@ -20,7 +20,7 @@ class ThoughtPattern extends Component {
                 color= "dark"
                 style= {{marginBottom: '2rem'}}
                 onClick={()=>{
-                    const name = prompt ('Ever thought');
+                    const name = prompt ('Record a thought');
                     if(name){
                         this.setState(state=>({
                             thoughts: [...state.thoughts, {id: uuid(), name}]
@@ -28,6 +28,17 @@ class ThoughtPattern extends Component {
                     }
                 }}>Record your daily thought pattens
                 </Button>
+                <ListGroup>
+                    <TransitionGroup className= "thought-list">
+                        {thoughts.map(({id, name})=> (
+                            <CSSTransition key={id} timeout= {500} classNames="fade">
+                                <ListGroupItem>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
             </Container>
         );
     }
