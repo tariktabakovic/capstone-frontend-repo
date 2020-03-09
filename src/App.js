f99383a01013be7daed70402903859b876d8f2c6
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 04:23:16 -0400

    implements ThoughtPatterns component into the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import AppNavBar from './components/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import ThoughtPatterns from './components/ThoughtPatterns';
 class App extends React.Component {
   
   render(){
     return(
       <div className = "App">
         <AppNavBar/>
+        <ThoughtPatterns/>
       </div>
     )}
 }
