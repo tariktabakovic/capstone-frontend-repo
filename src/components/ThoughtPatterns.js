e925771b4443bf7cf2b3ee72e211a38774e762bb
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 04:14:06 -0400

    creates component for thoughts

---
+++ src/components/ThoughtPatterns.js
@@ -0,0 +1,4 @@
+import React, {Component} from 'react';
+import {Container, ListGroup, ListGroupitem, Button} from 'reactstrap';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
+import uuid from 'uuid';
