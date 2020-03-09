7efdf2cf50ef1e33a23940c1b672e50bdea5d833
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 19:31:52 -0400

    creates initialState for thought reducer

---
+++ src/reducers/thoughtReducer.js
@@ -0,0 +1,10 @@
+import uuid from 'uuid';
+
+const initialState= {
+    thoughts: [
+        {id: uuid(), name: 'Be nice'},
+        {id: uuid(), name: 'Be fun'},
+        {id: uuid(), name: 'Be now'},
+        {id: uuid(), name: 'Be happy'}
+    ]
+}
