ecb302834525370fcc16e75f20c4b54b6c05f862
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 00:24:28 -0400

    removes old css for react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppNavBar from './components/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
 
 class App extends React.Component {
   
