e4c8a9f86ae896fd67dcd6c6f8ca8268c3597b56
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 04:21:03 -0400

    implements container and button in ThoughtPattern container, & sets state

--- src/components/ThoughtPatterns.js
+++ src/components/ThoughtPatterns.js
@@ -2,3 +2,34 @@ import React, {Component} from 'react';
 import {Container, ListGroup, ListGroupitem, Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import uuid from 'uuid';
+
+class ThoughtPattern extends Component {
+    state= {
+        thoughts: [
+            {id: uuid(), name: 'Be nice'},
+            {id: uuid(), name: 'Be fun'},
+            {id: uuid(), name: 'Be now'},
+            {id: uuid(), name: 'Be happy'}
+        ]
+    }
+    render(){
+        const {thoughts} = this.state;
+        return(
+            <Container>
+                <Button
+                color= "dark"
+                style= {{marginBottom: '2rem'}}
+                onClick={()=>{
+                    const name = prompt ('Ever thought');
+                    if(name){
+                        this.setState(state=>({
+                            thoughts: [...state.thoughts, {id: uuid(), name}]
+                        }))
+                    }
+                }}>Record your daily thought pattens
+                </Button>
+            </Container>
+        );
+    }
+}
+export default ThoughtPattern;
