c964d2e4fdd1db5bb7c60f572c0cb4100cca4f69
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 20:27:40 -0400

    imports thoughtModal & puts it in a reactstrap container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AppNavBar from './components/AppNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ThoughtPatterns from './components/ThoughtPatterns';
+import ThoughtModal from './components/ThoughtModal';
+import {Container} from 'reactstrap';
 
 import {Provider} from 'react-redux';
 import store from './store';
@@ -13,7 +15,10 @@ class App extends React.Component {
       <Provider store={store}>
         <div className = "App">
           <AppNavBar/>
-          <ThoughtPatterns/>
+          <Container>
+            <ThoughtModal/>
+            <ThoughtPatterns/>
+          </Container>
         </div>
       </Provider>
     )}
