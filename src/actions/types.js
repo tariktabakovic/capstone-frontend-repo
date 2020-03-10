3853e2bf6c21e8d9cec30c92ec59c523a54f3fcd
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 14:20:56 -0400

    creates new types for auth

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,14 @@
 export const GET_THOUGHTS = 'GET_THOUGHTS';
 export const ADD_THOUGHT = 'ADD_THOUGHT';
 export const DELETE_THOUGHT = 'DELETE_THOUGHT';
-export const THOUGHTS_LOADING = "THOUGHTS_LOADING"
+export const THOUGHTS_LOADING = "THOUGHTS_LOADING";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
+export const GET_ERRORS = "GET_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
