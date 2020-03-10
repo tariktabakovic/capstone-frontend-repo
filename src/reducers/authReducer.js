e568391c7b3b0996122f7ee87bce10ab8f66a501
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 14:34:02 -0400

    implements user_loading & user_loaded cases

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -17,3 +17,21 @@ const initialState = {
     isLoading: false,
     user: null
 }
+
+export default function (state= initialState, action){
+    switch(action.type){
+        case USER_LOADING:
+            return {
+                ...state,
+                isLoading: true
+            };
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+        };
+        
+    }
+}
