53a3fd20555434524dcfad90cafe6fd090d8e078
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 14:30:49 -0400

    imports action types

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+import {
+    USER_LOADED,
+    USER_LOADING,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    LOGOUT_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from '../actions/types'
