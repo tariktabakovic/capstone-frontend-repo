05f92eebf60a7fc54feb841d5db911f51f566f13
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 14:57:38 -0400

    checks token & loads user

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -11,3 +11,32 @@ import {
 } from './types';
 
 // Check token & load user
+export const loadUser = () => (dispatch, getState) => {
+    // User loading
+    dispatch({type: USER_LOADING});
+
+    // Get token from localStorage
+    const token = getState().auth.token;
+
+    // Headers
+    const config = {
+        headers: {
+            "Content-type": "application/json"
+        }
+    }
+
+    // If token, then add to headers
+    if(token){
+        config.headers['x-auth-token'] = token;
+    }
+    axios.get('/api/auth/user', config)
+        .then(res => dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        }))
+        .catch(err => {
+            dispatch({
+                type: AUTH_ERROR
+            });
+        });
+}
