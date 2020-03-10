099c9a10b5aba55dd1f081e5a885daa3dd3df99f
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 14:22:40 -0400

    imports auth and error reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import {combineReducers} from 'redux';
 import thoughtReducer from './thoughtReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
-    thought: thoughtReducer
+    thought: thoughtReducer,
+    error: errorReducer,
+    auth: authReducer
 })
