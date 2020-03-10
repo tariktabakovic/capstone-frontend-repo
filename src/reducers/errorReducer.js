a14bce5d02b77be8fcd0fc5c009b0974761cae88
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 14:25:48 -0400

    imports error actions & implements initialState in function

---
+++ src/reducers/errorReducer.js
@@ -0,0 +1,11 @@
+import {GET_ERRORS, CLEAR_ERRORS}from '../actions/types';
+
+const initialState= {
+    msg: {},
+    status: null,
+    id: null
+}
+
+export default function (state= initialState, action){
+
+}
