e807419ac76812823943d292f81f25cf7e82bdae
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 17:37:56 -0400

    passes tokenConfig to add and delete thought

--- src/actions/thoughtActions.js
+++ src/actions/thoughtActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import {GET_THOUGHTS, ADD_THOUGHT, DELETE_THOUGHT, THOUGHTS_LOADING} from './types';
+import {tokenConfig} from './authActions';
+import {returnErrors} from './errorActions';
 
 export const getThoughts = () => dispatch => {
     dispatch(setThoughtsLoading());
@@ -9,29 +11,32 @@ export const getThoughts = () => dispatch => {
             dispatch({
                 type: GET_THOUGHTS,
                 payload: res.data
-                })
-            )
+                }))
+        .catch(err => 
+            dispatch(returnErrors(err.response.data, err.response.status)))
 }
 
-export const addThought = thought => dispatch => {
+export const addThought = thought => (dispatch, getState) => {
     axios
-        .post('/api/dailythoughts', thought)
+        .post('/api/dailythoughts', thought, tokenConfig(getState))
         .then(res =>
             dispatch({
                 type: ADD_THOUGHT,
                 payload: res.data
-            })
-        )   
+            }))  
+        .catch(err => 
+            dispatch(returnErrors(err.response.data, err.response.status))) 
 }
 
-export const deleteThought = id => dispatch => {
+export const deleteThought = id => (dispatch, getState) => {
     axios
-        .delete(`/api/dailythoughts/${id}`)
+        .delete(`/api/dailythoughts/${id}`, tokenConfig(getState))
         .then(res => dispatch({
             type: DELETE_THOUGHT,
             payload: id
-        })
-    )
+        }))
+    .catch(err => 
+        dispatch(returnErrors(err.response.data, err.response.status)))
 }
 
 
