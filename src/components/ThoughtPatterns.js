8c49c1e2d6e39e927e951b592d25d28899581c3e
“tariktabakovic” <tariktab97@gmail.com>
2020-03-10 12:27:53 -0400

    implements id from DB

--- src/components/ThoughtPatterns.js
+++ src/components/ThoughtPatterns.js
@@ -20,27 +20,15 @@ class ThoughtPattern extends Component {
         const {thoughts} = this.props.thought;
         return(
             <Container>
-                <Button
-                color= "dark"
-                style= {{marginBottom: '2rem'}}
-                onClick={()=>{
-                    const name = prompt ('Record a thought');
-                    if(name){
-                        this.setState(state=>({
-                            thoughts: [...state.thoughts, {id: uuid(), name}]
-                        }))
-                    }
-                }}>Record your daily thought pattens
-                </Button>
                 <ListGroup>
                     <TransitionGroup className= "thought-list">
-                        {thoughts.map(({id, name})=> (
-                            <CSSTransition key={id} timeout= {500} classNames="fade">
+                        {thoughts.map(({_id, name})=> (
+                            <CSSTransition key={_id} timeout= {500} classNames="fade">
                                 <ListGroupItem>
                                     <Button className= "remove-btn"
                                     color="danger"
                                     size="sm"
-                                    onClick={this.onDeleteClick.bind(this, id)}
+                                    onClick={this.onDeleteClick.bind(this, _id)}
                                     >&times;</Button>
                                     {name}
                                 </ListGroupItem>
