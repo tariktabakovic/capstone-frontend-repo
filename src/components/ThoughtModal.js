b545a1f17851e2d2cbb9a883a8c1fe4f4b3d9cef
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 20:13:37 -0400

    imports from reactstrap

---
+++ src/components/ThoughtModal.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {
+    Button, 
+    Modal,
+    ModalHeader,
+    ModalBody,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from 'reactstrap';
