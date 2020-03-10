4c9ebabb4b830632fbace989cbf38fbc74b2c648
“tariktabakovic” <tariktab97@gmail.com>
2020-03-09 20:19:01 -0400

    implements button into the modal

--- src/components/ThoughtModal.js
+++ src/components/ThoughtModal.js
@@ -14,7 +14,29 @@ import {connect} from 'react-redux';
 import {addThought} from '../actions/thoughtActions';
 
 class ThoughtModal extends Component{
+    state= {
+        modal: false,
+        name: ''
+    }
 
+    toggle = () =>{
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <Button
+                color= "dark"
+                style= {{marginBottom: "2rem"}}
+                onClick= {this.toggle}>
+                    Record your daily thoughts
+                </Button>
+            </div>
+        );
+    }
 }
 
 export default connect ()(ThoughtModal);
