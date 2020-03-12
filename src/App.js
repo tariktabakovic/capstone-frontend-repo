034e7c80eafc65b19d66e18d924f111a1a3fc016
“tariktabakovic” <tariktab97@gmail.com>
2020-03-12 09:22:06 -0400

    comments out routes

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ThoughtPatterns from './components/ThoughtPatterns';
 import ThoughtModal from './components/ThoughtModal';
 import {Container} from 'reactstrap';
-import {BrowserRouter} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
-
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
+import Welcome from './components/Welcome';
 import {Provider} from 'react-redux';
 import store from './store';
 import {loadUser} from './actions/authActions'
-
+import {LoginModalAttempt} from './components/auth/LoginModalAttempt';
+import {LoginModal} from './components/auth/LoginModal';
+import Game from './components/Game.js';
 class App extends React.Component {
   componentDidMount(){
     store.dispatch(loadUser());
@@ -19,15 +21,20 @@ class App extends React.Component {
   render(){
     return(
       <Provider store={store}>
-        <Router>
+        {/* <Router> */}
+          {/* <Switch> */}
           <div className = "App">
+            {/* <Route exact path= "/" component={AppNavBar}></Route> */}
             <AppNavBar/>
+            {/* <Route exact path= "/home" render={(routeProps)=>(<Container><ThoughtModal/><ThoughtPatterns/></Container>)}/> */}
             <Container>
+              {/* <Game/> */}
               <ThoughtModal/>
               <ThoughtPatterns/>
-            </Container>
-          </div>
-        </Router>
+            </Container> 
+            </div>
+          {/* </Switch> */}
+        {/* // </Router> */}
       </Provider>
     )}
 }
