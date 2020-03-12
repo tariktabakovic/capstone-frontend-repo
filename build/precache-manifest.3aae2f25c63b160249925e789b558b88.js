17a7d50bda1b1d78e4e854cc65d9a666f8a0704a
“tariktabakovic” <tariktab97@gmail.com>
2020-03-12 12:09:54 -0400

    creates build

---
+++ build/precache-manifest.3aae2f25c63b160249925e789b558b88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b59ec9d31aaea1d63f93b0387b655b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554868d6d61673e7bf53",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ca598e2ad82afb753ae3",
+    "url": "/static/css/main.17318b87.chunk.css"
+  },
+  {
+    "revision": "554868d6d61673e7bf53",
+    "url": "/static/js/2.5e34b444.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.5e34b444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca598e2ad82afb753ae3",
+    "url": "/static/js/main.d4c1582f.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
